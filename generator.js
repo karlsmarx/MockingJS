5eb2e6c53fbc3921fd199700652c50a3c55200d8
Karlsmarx <karlmarxti@gmail.com>
2019-02-19 00:01:48 -0300

    Adding checks and validation to floating point numbers

--- generator.js
+++ generator.js
@@ -11,13 +11,22 @@
  * In future this will be compatible with other Databases.
  */
 
+// Contraints for check data types
+const SUPERIOR_LIMIT_BIT = 64; // Defines the max length of a bit entry
+
+const INFERIOR_INTEGER_LIMIT_DECIMAL = 1; // Defines the minumum integer digits for DECIMAL type
+const SUPERIOR_INTEGER_LIMIT_DECIMAL = 64; // Defines the maximum integer digits for DECIMAL type
+
+const INFERIOR_FRACTIONAL_LIMIT_DECIMAL = 1; // Defines the minumum fractional digits for DECIMAL type
+const SUPERIOR_FRACTIONAL_LIMIT_DECIMAL = 29; // Defines the maximum fractional digits for DECIMAL type
+
 // Binary sequence
 const BIT = (line) => {
 	const regex = /(BIT\(\d{1,2}\))/g; // Regular expression to match if line has BIT(m) declaration and get the m.
 
 	if (regex.test(line)) {
 		const size = parseInt(line.match(regex)[0].match(/\d{1,2}/g), 10);
-		if (size > 65) throw Error("ERR_INVALID_SIZE");
+		if (size > SUPERIOR_LIMIT_BIT) throw Error("ERR_INVALID_SIZE");
 
 		const elements = line.trim().split(" ");
 
@@ -40,7 +49,7 @@ const BIT = (line) => {
 };
 
 /**
- * In MySQL integer values could be declared with a display otpion, that complete the value with zeros
+ * In MySQL integer values could be declared with a display option, that complete the value with zeros
  * if is minor, but for logical operations it's irrelevant. The capture group (\(\d+\)) in all regexs get this
  * value, but for now they are ignored.
  */
@@ -98,7 +107,7 @@ const SMALLINT = (line) => {
 			},
 		};
 
-		// Anotates the max and min size for signed and unsigned TINYINT
+		// Anotates the max and min size for signed and unsigned SMALLINT
 		if (elements.includes("UNSIGNED")) {
 			info.size = {
 				max: 65334,
@@ -134,7 +143,7 @@ const MEDIUMINT = (line) => {
 			},
 		};
 
-		// Anotates the max and min size for signed and unsigned TINYINT
+		// Anotates the max and min size for signed and unsigned MEDIUMINT
 		if (elements.includes("UNSIGNED")) {
 			info.size = {
 				max: 16777215,
@@ -170,7 +179,7 @@ const INT = (line) => {
 			},
 		};
 
-		// Anotates the max and min size for signed and unsigned TINYINT
+		// Anotates the max and min size for signed and unsigned INT
 		if (elements.includes("UNSIGNED")) {
 			info.size = {
 				max: 4294967296,
@@ -209,7 +218,7 @@ const BIGINT = (line) => {
 			},
 		};
 
-		// Anotates the max and min size for signed and unsigned TINYINT
+		// Anotates the max and min size for signed and unsigned BIGINT
 		if (elements.includes("UNSIGNED")) {
 			info.size = {
 				max: (2 ** 64) - 1,
@@ -253,6 +262,183 @@ const BOOL = (line) => {
 // For keep isomorphism with MySQL
 const BOOLEAN = BOOL;
 
+/**
+ * Floating point declarations
+ */
+const DECIMAL = (line) => {
+	// The logest regex to match all possible equivalent types
+	const regex = /(DECIMAL(\(\d+(,\d+)?\))?)|(\bDEC(\(\d+(,\d+)?\))?)|(\bNUMERIC(\(\d+(,\d+)?\))?)|(\bFIXED(\(\d+(,\d+)?\))?)/g;
+
+	if (regex.test(line)) {
+		const elements = line.trim().split(" ");
+
+		const info = {
+			name: elements[0].replace(/(\'|`)/g, ""), // eslint-disable-line
+			type: "float",
+			nullable: !elements.includes("NOT"),
+
+			/**
+			 * Validate only numbers with explicit point, to keep clear that is a floating point number,
+			 * because of that decision, the input must be a string or will be lost the fractional part
+			 * when is [somenumber].0
+			 */
+			validator: (decimal) => {
+				const validator = /(-?\d+)\.(\d+)/g;
+				const result = validator.exec(decimal);
+
+				if (!result) return false;
+
+				// Check for max constraints, the minumum is get by regex
+				if (result[1].length > SUPERIOR_INTEGER_LIMIT_DECIMAL) throw Error("ERR_INTEGER_PART_TOO_BIG");
+				if (result[2].length > SUPERIOR_FRACTIONAL_LIMIT_DECIMAL) throw Error("ERR_FRACTIONAL_PART_TOO_BIG");
+
+				return true;
+			},
+		};
+
+		/**
+		 * Anotates the max and min size for signed and unsigned DECIMAL.
+		 * Unsigned numbers only deactivate negative parts in this case.
+		 */
+		if (elements.includes("UNSIGNED")) {
+			info.size = {
+				max: (10 ** 65) - (10 ** -30), // A number with 64 nines in integer part and 29 nines in fractional part
+				min: 0,
+			};
+		} else {
+			info.size = {
+				max: (10 ** 65) - (10 ** -30),
+				min: -(10 ** 65) - (10 ** -30),
+			};
+		}
+
+		return info;
+	}
+
+	return false;
+};
+
+// To keep compatibility
+const DEC = DECIMAL;
+const NUMERIC = DECIMAL;
+const FIXED = DECIMAL;
+
+/**
+ * The FLOAT and DOUBLE type in MySQL is defined by a implementation
+ * of IEEE 754, this means that number could be virtually Infinity,
+ * if the mantissa is 0. To keep that in mind the max value
+ * in info is Infinity.
+ *
+ * In fact every number in JS could be represented by a double precision
+ * floating point number, and when his limit is reached this came a Infinity.
+ */
+const FLOAT = (line) => {
+	const regex = /(FLOAT(\(\d+(,\d+)?\))?)/g;
+
+	if (regex.test(line)) {
+		const elements = line.trim().split(" ");
+
+		const info = {
+			name: elements[0].replace(/(\'|`)/g, ""), // eslint-disable-line
+			type: "float",
+			nullable: !elements.includes("NOT"),
+
+			/**
+			 * Validate only numbers with explicit point, to keep clear that is a floating point number,
+			 * because of that decision, the input must be a string or will be lost the fractional part
+			 * when is [somenumber].0
+			 *
+			 * Not validate the max digits cause is calculated only by his bits. (Are really floating)
+			 */
+			validator: (float) => {
+				const validator = /(-?\d+)\.(\d+)/g;
+				const result = validator.exec(float);
+
+				if (!result) return false;
+				return true;
+			},
+		};
+
+		/**
+		 * Anotates the max and min size for signed and unsigned FLOAT.
+		 * Unsigned numbers only deactivate negative parts in this case.
+		 */
+		if (elements.includes("UNSIGNED")) {
+			info.size = {
+				max: Infinity, // Depends of hardware, without defined limit
+				min: 0,
+			};
+		} else {
+			info.size = {
+				max: Infinity,
+				min: -Infinity,
+			};
+		}
+
+		return info;
+	}
+
+	return false;
+};
+
+/**
+ * Essentially this is the same check of FLOAT, only the regex is different,
+ * but in future the method will help to calculate the minimum, max and
+ * medium values of each row in a table.
+ */
+const DOUBLE = (line) => {
+	const regex = /(\bDOUBLE PRECISION(\(\d+(,\d+)?\))?)|(DOUBLE(\(\d+(,\d+)?\))?)|(\bREAL(\(\d+(,\d+)?\))?)/g;
+
+	if (regex.test(line)) {
+		const elements = line.trim().split(" ");
+
+		const info = {
+			name: elements[0].replace(/(\'|`)/g, ""), // eslint-disable-line
+			type: "float",
+			nullable: !elements.includes("NOT"),
+
+			/**
+			 * Validate only numbers with explicit point, to keep clear that is a floating point number,
+			 * because of that decision, the input must be a string or will be lost the fractional part
+			 * when is [somenumber].0
+			 *
+			 * Not validate the max digits cause is calculated only by his bits. (Are really floating)
+			 */
+			validator: (float) => {
+				const validator = /(-?\d+)\.(\d+)/g;
+				const result = validator.exec(float);
+
+				if (!result) return false;
+				return true;
+			},
+		};
+
+		/**
+		 * Anotates the max and min size for signed and unsigned FLOAT.
+		 * Unsigned numbers only deactivate negative parts in this case.
+		 */
+		if (elements.includes("UNSIGNED")) {
+			info.size = {
+				max: Infinity, // Depends of hardware, without defined limit
+				min: 0,
+			};
+		} else {
+			info.size = {
+				max: Infinity,
+				min: -Infinity,
+			};
+		}
+
+		return info;
+	}
+
+	return false;
+};
+
+// To keep compatibility
+const DOUBLE_PRECISION = DOUBLE;
+const REAL = DOUBLE;
+
 module.exports = {
 	BIT,
 	TINYINT,
@@ -263,4 +449,12 @@ module.exports = {
 	BIGINT,
 	BOOL,
 	BOOLEAN,
+	DECIMAL,
+	DEC,
+	NUMERIC,
+	FIXED,
+	FLOAT,
+	DOUBLE,
+	DOUBLE_PRECISION,
+	REAL,
 };
--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const generator = require("./generator.js");
 
 /* eslint-disable*/
-const sqlStatement = "CREATE TABLE IF NOT EXISTS `mydb`.`table1`( \n \
-`id`INT NOT NULL, \n \
-`table1col` BIT(32) NULL, \n \
-`table1col1` TINYINT(1) UNSIGNED NULL, \n \
-	`table1col1` TINYINT UNSIGNED NULL, \n \
-	`table1col1` TINYINT UNSIGNED NOT NULL, \n \
-	`table1col1` TINYINT NULL, \n \
-`table1col2` SMALLINT(1) UNSIGNED NULL, \n \
-	`table1col2` SMALLINT UNSIGNED NULL, \n \
-`table1col3` MEDIUMINT(1) NULL, \n \
-	`table1col3` MEDIUMINT NULL, \n \
-`table1col4` INT(1) NULL, \n \
-	`table1col4` INT NULL, \n \
-`table1col4` INTEGER(1) NULL, \n \
-	`table1col4` INTEGER NULL, \n \
-`table1col5` BIGINT(1) NULL, \n \
-	`table1col5` BIGINT NULL, \n \
-`table1col5` BOOL NULL, \n \
-`table1col5` BOOLEAN NULL, \n \
-PRIMARY KEY(`id`)) \n \
-ENGINE = InnoDB;";
+const sqlStatement = "CREATE TABLE IF NOT EXISTS `mydb`.`table1` ( \
+	`idtable1` INT NOT NULL,\
+	`decimal` DECIMAL(66,31) NULL,\
+	`decimal2` DECIMAL NULL,\
+	`dec` DEC NULL,\
+	`numeric`NUMERIC NULL,\
+	`fixed` FIXED NULL,\
+	PRIMARY KEY (`idtable1`))\
+  ENGINE = InnoDB";
 
 const lines = sqlStatement.split("\n");
 
 lines.forEach((line) => {
-	const result = generator.BIGINT(line);
+	const result = generator.DECIMAL(line);
 
 	if (result) {
-		console.log(result);
+		console.log(result.validator(1.1));
 	}
 });
